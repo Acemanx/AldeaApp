a67cc5fc5846341819b2d37a17a8d7d91f8c9daf
unknown <acevedo1500@gmail.com>
2017-11-25 18:43:51 -0500

    Commit3

---
+++ AldeaApp/Models/Respuesta2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AldeaApp.Models
+{
+    public class Respuesta2
+    {
+        public string mensaje { get; set; }
+        public string rol { get; set; }
+    }
+}
